62c7d442044402f161b12a4b61d461d8de150e13
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-11-15 16:13:23 -0600

    added test for perimeter and area method

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,13 +1,15 @@
 public class Triangle {
-    public static float Perimeter(float aSide, float bSide, float cSide) {
+    public static float num;
+
+    public static float perimeter(float aSide, float bSide, float cSide) {
         return aSide+bSide+cSide;
     };
 
-    public static float Area(float bSide, float height) {
+    public static float area(float bSide, float height) {
         return (float) .5*bSide*height;
     };
 
-    public static float Pythagoras(float aSide, float bSide, float cSide) {
+    public static float pythagoras(float aSide, float bSide, float cSide) {
         return (float) Math.sqrt((aSide*aSide) + (bSide*bSide));
     };
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -1,3 +1,19 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 public class TriangleTest {
+
+    @Test
+    public void testPerimeter() {
+        assertEquals(30, Triangle.perimeter(5, 10, 15), 0);
+        assertNotEquals(35, Triangle.perimeter(5, 10, 15), 0);
+    }
+
+    @Test
+    public void testArea() {
+        assertEquals(75, Triangle.area(10, 15), 0);
+        assertNotEquals(90, Triangle.area(10, 15), 0);
+    }
 
 }
